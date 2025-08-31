ec7cd9c8fed207878e13bc72a699126ed976b326
paukstelis <paukstel@umd.edu>
2025-08-31 11:37:24 -0400

    Update le_install_mac.sh
    already had simplethreads

--- le_install_mac.sh
+++ le_install_mac.sh
@@ -17,8 +17,6 @@ python3 -m venv ~/OctoPrint
 
 ~/OctoPrint/bin/pip install https://github.com/paukstelis/LE_Scanning/archive/refs/heads/main.zip
 
-~/OctoPrint/bin/pip install https://github.com/paukstelis/OctoPrint-Simplethreads/archive/refs/heads/main.zip
-
 ~/OctoPrint/bin/pip install https://github.com/paukstelis/LE-RoseEngine/archive/refs/heads/main.zip
 
 
